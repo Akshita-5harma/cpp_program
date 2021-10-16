d262ba2f9b02dd5563006d55235a30ba01483ecf
Akshita-5harma <87754597+Akshita-5harma@users.noreply.github.com>
2021-10-16 14:51:11 +0530

    Update 01-greatest_of_3_integers.cpp
    logical error correction

--- 03-flow_of _control/01-greatest_of_3_integers.cpp
+++ 03-flow_of _control/01-greatest_of_3_integers.cpp
@@ -1,32 +1,31 @@
-// Accept 3 integers and print the largest of three using only if
-/* 
-17 Jun 21
-Mohammad Shahbaz Alam
-Program 9.1
-*/
+// Accept 3 integers and print the largest of three using only if and else
+
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int a,b,c,max;
     cout<< "\nEnter 3 intergers " ;
     cin >> a >> b >> c ;
     
-    max = a;
-    
-    if(b>max) 
+    if( (b>a) && (b>c) ) 
     {
         max = b;
     }
     
-    if(c>max)
+    else if( (c>a) && (c>b) )
     {
         max = c;
     }
+    
+    else 
+    {
+        max = a;
+    }
 
-    cout<< "The maximum of three digits " << a << ", " << b << ", " << c << " is " << max <<"\n\n";
-     
+    cout<< "The maximum of three digits "<<a<<", "<<b<<", "<<c<<" is "<<max<<endl;
     return 0;
 }
